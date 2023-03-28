7e9b9e1cfdcd50c196b6f71eb7d61d858a6bc901
flyingman2401 <flyingman24012001@gmail.com>
2023-03-29 00:45:10 +0700

    Modify QR Code Scanner

--- lib/src/presentation/qrcodescan_page.dart
+++ lib/src/presentation/qrcodescan_page.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:qr_code_scanner/qr_code_scanner.dart';
+import 'package:flutter_svg/flutter_svg.dart';
 import 'dart:io';
 
 class QRCodeScanPage extends StatefulWidget {
@@ -35,6 +36,24 @@ class QRCodeScanPageState extends State<QRCodeScanPage> {
   void _onQRViewCreated(QRViewController controller) {
     this.controller = controller;
     controller.scannedDataStream.listen((scanData) {
+      String stringData = scanData.code!;
+      showDialog<String>(
+        context: context,
+        builder: (BuildContext context) => AlertDialog(
+          title: const Text('QR scanned!'),
+          content: Text('Result: $stringData'),
+          actions: <Widget>[
+            TextButton(
+              onPressed: () => Navigator.pop(context, 'Cancel'),
+              child: const Text('Cancel'),
+            ),
+            TextButton(
+              onPressed: () => Navigator.pop(context, 'OK'),
+              child: const Text('OK'),
+            ),
+          ],
+        ),
+      );
       setState(() {
         result = scanData.code!;
       });
@@ -47,51 +66,64 @@ class QRCodeScanPageState extends State<QRCodeScanPage> {
       body: Container(
         width: MediaQuery.of(context).size.width,
         height: MediaQuery.of(context).size.height,
-        child: Column(
-          mainAxisAlignment: MainAxisAlignment.start,
-          crossAxisAlignment: CrossAxisAlignment.start,
+        child: Stack(
           children: [
-            Container(height: 24.0),
             Container(
-              width: 48.0,
-              height: 48.0,
-              decoration: const BoxDecoration(
-                color: Colors.green
-              ),
-              // padding: EdgeInsets.only(bottom: 12.0, left: 12.0),
-              child: BackButton(
-                onPressed: () => Navigator.pop(context),
+              width: MediaQuery.of(context).size.width,
+              height: MediaQuery.of(context).size.height,
+              child: QRView(
+                key: qrKey, 
+                onQRViewCreated: _onQRViewCreated,
+                overlay: QrScannerOverlayShape(
+                  borderColor: Colors.white,
+                  borderWidth: 8.0,
+                  borderRadius: 30.0,
+                  borderLength: 50.0
+                ),
               ),
             ),
             Container(
               width: MediaQuery.of(context).size.width,
-              height: MediaQuery.of(context).size.height - 24.0 - 48.0,
-              decoration: BoxDecoration(
-                color: Colors.blue
-              ),
+              height: MediaQuery.of(context).size.height,
               child: Column(
+                mainAxisAlignment: MainAxisAlignment.spaceBetween,
                 children: [
-                  Expanded(
-                    flex: 5,
-                    child: QRView(
-                      key: qrKey,
-                      onQRViewCreated: _onQRViewCreated,
-                    )
+                  Row(
+                    mainAxisAlignment: MainAxisAlignment.start,
+                    crossAxisAlignment: CrossAxisAlignment.center,
+                    children: [
+                      BackButton(
+                        onPressed: () => {Navigator.pop(context, true)},
+                      ),
+                      Text('Quét mã QR', style: Theme.of(context).textTheme.titleLarge, )
+                    ],
                   ),
-                  Expanded(
-                    flex: 1,
-                    child: Center(
-                      child: Text("Scan result: $result"),
+                  Container(
+                    width: MediaQuery.of(context).size.width,
+                    height: MediaQuery.of(context).size.height / 3,
+                    decoration: BoxDecoration(
+                      // color: Colors.blue,
+                    ),
+                    child: IconButton(
+                      style: const ButtonStyle(
+                        backgroundColor: MaterialStatePropertyAll<Color>(Colors.black),
+
+                      ),
+                      icon: SvgPicture.asset(
+                        'lib/images/flash-off.svg',
+                        width: 24.0,
+                        height: 24.0,
+                        color: Colors.white,
+                      ),
+                      onPressed: () => {},
                     ),
                   )
                 ],
-              )
-            )
-            
-
+              ),
+            ),
           ],
         )
-      ),
+      )
     );
   }
 }
