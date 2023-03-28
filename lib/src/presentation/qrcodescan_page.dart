eecbb92e5208c3529f4db5d3405e8d8d32e8e2d3
flyingman2401 <flyingman24012001@gmail.com>
2023-03-28 20:26:14 +0700

    Modify QR Code Scanner

--- lib/src/presentation/qrcodescan_page.dart
+++ lib/src/presentation/qrcodescan_page.dart
@@ -1,146 +1,85 @@
 import 'package:flutter/material.dart';
+import 'package:qr_code_scanner/qr_code_scanner.dart';
 
-class QRCodeScanPage extends StatelessWidget {
-  const QRCodeScanPage({Key? key, required this.overlayColour})
-      : super(key: key);
+class QRCodeScanPage extends StatefulWidget {
+  const QRCodeScanPage({super.key});
 
-  final Color overlayColour;
+  @override
+  QRCodeScanPageState createState() => QRCodeScanPageState();
+}
+
+class QRCodeScanPageState extends State<QRCodeScanPage> {
+
+  final GlobalKey qrKey = GlobalKey(debugLabel: "QR");
+  QRViewController? controller;
+  String result = "";
+
+  @override
+  void dispose() {
+    controller?.dispose();
+    super.dispose();
+  }
+
+  void _onQRViewCreated(QRViewController controller) {
+    this.controller = controller;
+    controller.scannedDataStream.listen((scanData) {
+      setState(() {
+        result = scanData.code!;
+      });
+    });
+  }
 
   @override
   Widget build(BuildContext context) {
-    double scanArea = (MediaQuery.of(context).size.width < 400 ||
-            MediaQuery.of(context).size.height < 400)
-        ? 200.0
-        : 330.0;
-    return Stack(children: [
-      ColorFiltered(
-        colorFilter: ColorFilter.mode(
-            overlayColour, BlendMode.srcOut), // This one will create the magic
-        child: Stack(
+    return Scaffold(
+      body: Container(
+        width: MediaQuery.of(context).size.width,
+        height: MediaQuery.of(context).size.height,
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.start,
+          crossAxisAlignment: CrossAxisAlignment.start,
           children: [
+            Container(height: 24.0),
             Container(
+              width: 48.0,
+              height: 48.0,
               decoration: const BoxDecoration(
-                  color: Colors.red,
-                  backgroundBlendMode: BlendMode
-                      .dstOut), // This one will handle background + difference out
-            ),
-            Align(
-              alignment: Alignment.center,
-              child: Container(
-                height: scanArea,
-                width: scanArea,
-                decoration: BoxDecoration(
-                  color: Colors.red,
-                  borderRadius: BorderRadius.circular(20),
-                ),
+                color: Colors.green
+              ),
+              // padding: EdgeInsets.only(bottom: 12.0, left: 12.0),
+              child: BackButton(
+                onPressed: () => Navigator.pop(context),
               ),
             ),
+            Container(
+              width: MediaQuery.of(context).size.width,
+              height: MediaQuery.of(context).size.height - 24.0 - 48.0,
+              decoration: BoxDecoration(
+                color: Colors.blue
+              ),
+              child: Column(
+                children: [
+                  Expanded(
+                    flex: 5,
+                    child: QRView(
+                      key: qrKey,
+                      onQRViewCreated: _onQRViewCreated,
+                    )
+                  ),
+                  Expanded(
+                    flex: 1,
+                    child: Center(
+                      child: Text("Scan result: $result"),
+                    ),
+                  )
+                ],
+              )
+            )
+            
+
           ],
-        ),
+        )
       ),
-      Align(
-        alignment: Alignment.center,
-        child: CustomPaint(
-          foregroundPainter: BorderPainter(),
-          child: SizedBox(
-            width: scanArea + 25,
-            height: scanArea + 25,
-          ),
-        ),
-      ),
-    ]);
-  }
-}
-
-// Creates the white borders
-class BorderPainter extends CustomPainter {
-  @override
-  void paint(Canvas canvas, Size size) {
-    const width = 4.0;
-    const radius = 20.0;
-    const tRadius = 3 * radius;
-    final rect = Rect.fromLTWH(
-      width,
-      width,
-      size.width - 2 * width,
-      size.height - 2 * width,
-    );
-    final rrect = RRect.fromRectAndRadius(rect, const Radius.circular(radius));
-    const clippingRect0 = Rect.fromLTWH(
-      0,
-      0,
-      tRadius,
-      tRadius,
-    );
-    final clippingRect1 = Rect.fromLTWH(
-      size.width - tRadius,
-      0,
-      tRadius,
-      tRadius,
-    );
-    final clippingRect2 = Rect.fromLTWH(
-      0,
-      size.height - tRadius,
-      tRadius,
-      tRadius,
-    );
-    final clippingRect3 = Rect.fromLTWH(
-      size.width - tRadius,
-      size.height - tRadius,
-      tRadius,
-      tRadius,
-    );
-
-    final path = Path()
-      ..addRect(clippingRect0)
-      ..addRect(clippingRect1)
-      ..addRect(clippingRect2)
-      ..addRect(clippingRect3);
-
-    canvas.clipPath(path);
-    canvas.drawRRect(
-      rrect,
-      Paint()
-        ..color = Colors.white
-        ..style = PaintingStyle.stroke
-        ..strokeWidth = width,
     );
   }
-
-  @override
-  bool shouldRepaint(CustomPainter oldDelegate) {
-    return false;
-  }
-}
-
-class BarReaderSize {
-  static double width = 200;
-  static double height = 200;
-}
-
-class OverlayWithHolePainter extends CustomPainter {
-  @override
-  void paint(Canvas canvas, Size size) {
-    final paint = Paint()..color = Colors.black54;
-    canvas.drawPath(
-        Path.combine(
-          PathOperation.difference,
-          Path()..addRect(Rect.fromLTWH(0, 0, size.width, size.height)),
-          Path()
-            ..addOval(Rect.fromCircle(
-                center: Offset(size.width - 44, size.height - 44), radius: 40))
-            ..close(),
-        ),
-        paint);
-  }
-
-  @override
-  bool shouldRepaint(CustomPainter oldDelegate) {
-    return false;
-  }
-}
-
-@override
-bool shouldRepaint(CustomPainter oldDelegate) {
-  return false;
 }
--- lib/src/widgets/navigationbar_widget.dart
+++ lib/src/widgets/navigationbar_widget.dart
@@ -68,7 +68,7 @@ class NavigationBarWidgetState extends State<NavigationBarWidget> {
             Navigator.push(
               context,
               PageRouteBuilder(
-                pageBuilder: (_, __, ___) => const QRCodeScanPage(overlayColour: Colors.blue,),
+                pageBuilder: (_, __, ___) => QRCodeScanPage(),
                 transitionDuration: const Duration(milliseconds: 30),
                 transitionsBuilder: (_, a, __, c) => FadeTransition(opacity: a, child: c),
               )
